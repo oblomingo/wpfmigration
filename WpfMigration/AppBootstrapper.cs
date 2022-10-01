1a1e4e8c317651fbfba1abe0b7b963de316f9439
Aleksandr Suchovarov <aleksandr.suchovarov@surfsharkteam.com>
2022-10-01 20:57:05 +0300

    Initial commit

--- WpfMigration/AppBootstrapper.cs
+++ WpfMigration/AppBootstrapper.cs
@@ -1,7 +1,18 @@
-﻿namespace WpfMigration
+﻿using Caliburn.Micro;
+using WpfMigration.Dashboard;
+
+namespace WpfMigration
 {
-    public class AppBootstrapper
+    public class AppBootstrapper : BootstrapperBase
     {
-        
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+ 
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
     }
 }
